39e844e5ea40a1e293785ea058ad1fdb8fbe18e5
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2026-01-30 05:59:50 -0600

    Enable colors for middlewares/loggerMiddleware.js

--- middlewares/loggerMiddleware.js
+++ middlewares/loggerMiddleware.js
@@ -1,7 +1,10 @@
+const colors = require('colors');
+colors.enabled = true;
+
 exports.loggerMiddleware = (str) => {
    const currentDate = new Date();
-   const formattedDate = `${currentDate.getMonth() + 1}/${currentDate.getDate()}/${currentDate.getFullYear()} @ ${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`;
+   const formattedDate = `${currentDate.getMonth() + 1}/${currentDate.getDate()}/${currentDate.getFullYear()} @ ${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`.bold.italic.blue;
 
-   console.log(`loggerMiddleware..${str}...${formattedDate}`);
+   console.log(`loggerMiddleware..${str}...${formattedDate}`.bold.italic.blue);
 
 }
